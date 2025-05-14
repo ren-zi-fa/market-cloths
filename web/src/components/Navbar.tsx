a348a91376dcc1abceecb5e5636ff4f4c918f110
renzi febriandika <renzifebriandika923@gmail.com>
2025-05-15 00:30:15 +0700

    feat: implement responsive Navbar component and integrate with layout
    - Added Navbar component with responsive design using Sheet for mobile view.
    - Integrated Navbar into the RootLayout.
    - Created constants for navigation links.
    - Updated global styles to include new color variables and font imports.
    - Refactored Home page layout to prepare for future content.
    - Introduced utility functions for class name management.
    - Added UI components for buttons, labels, selects, sheets, and drawers.
    - Included logo asset for branding.

---
+++ web/src/components/Navbar.tsx
@@ -0,0 +1,84 @@
+import {
+   AlignLeft,
+   Heart,
+   Menu,
+   ShoppingBag,
+   Slack,
+   UserRound
+} from 'lucide-react'
+import { Nav_Links } from '@/constants/navbar'
+import {
+   Sheet,
+   SheetContent,
+   SheetTrigger,
+   SheetHeader,
+   SheetTitle,
+   SheetDescription,
+   SheetClose
+} from '@/components/ui/sheet'
+import Link from 'next/link'
+import Logom from '@/assets/logo.png'
+import Image from 'next/image'
+
+export default function Navbar() {
+   return (
+      <header className="sticky top-0 z-50 w-full px-2 md:px-8">
+         <div className="container flex h-16 items-center gap-4 mx-auto justify-between">
+            {/* Kiri: SheetTrigger dan Nav Links */}
+            <div className="flex items-center gap-3">
+               {/* SheetTrigger di kiri */}
+               <Sheet>
+                  <SheetTrigger className="md:hidden" asChild>
+                     <button>
+                        <AlignLeft />
+                     </button>
+                  </SheetTrigger>
+                  <SheetContent side="right" className="py-10">
+                     <SheetHeader>
+                        <SheetTitle>Menu</SheetTitle>
+                        <SheetDescription>Navigasi utama</SheetDescription>
+                     </SheetHeader>
+                     <div className="flex flex-col gap-6 mt-8">
+                        {Nav_Links.map((items, i) => (
+                           <SheetClose asChild key={i}>
+                              <Link
+                                 href={items.path}
+                                 className="hover:text-black font-medium text-xl text-purple-600"
+                              >
+                                 {items.title}
+                              </Link>
+                           </SheetClose>
+                        ))}
+                     </div>
+                  </SheetContent>
+               </Sheet>
+               <nav className="hidden md:flex items-center gap-6">
+                  {Nav_Links.map((item, i) => (
+                     <Link
+                        key={i}
+                        href={item.path}
+                        className="hover:text-purple-500 font-medium"
+                     >
+                        {item.title}
+                     </Link>
+                  ))}
+               </nav>
+            </div>
+            {/* Logo tengah hanya di desktop */}
+            <Link href="/" className="hidden md:flex">
+               <Image src={Logom} alt="logo" priority />
+            </Link>
+            {/* Kanan: Icon (desktop) */}
+            <div className="hidden md:flex items-center gap-4">
+               <Heart />
+               <ShoppingBag />
+               <UserRound />
+            </div>
+            {/* Logo kanan hanya di mobile */}
+            <Link href="/" className="flex md:hidden ml-auto">
+               <Image src={Logom} priority alt="logo" className="h-8 w-auto" />
+            </Link>
+         </div>
+      </header>
+   )
+}
---
+++ web/src/constants/navbar.tsx
@@ -0,0 +1,14 @@
+export const Nav_Links: { title: string; path: string }[] = [
+   {
+      title: 'Home',
+      path: '/'
+   },
+   {
+      title: 'Collections',
+      path: '/collections'
+   },
+   {
+      title: 'New',
+      path: '/new'
+   }
+] as const
