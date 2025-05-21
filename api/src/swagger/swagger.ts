9a85b16a27a96f55edfa36c6d28caa11c071d9b3
renzi febriandika <renzifebriandika923@gmail.com>
2025-05-21 15:04:37 +0700

    fix: path docs

--- api/src/swagger/swagger.ts
+++ api/src/swagger/swagger.ts
@@ -25,7 +25,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000', // sesuaikan URL
+        url: 'https://market-cloths.vercel.app', // sesuaikan URL
       },
     ],
     components: {
